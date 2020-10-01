d42842c170e503970dadd5bef5bc703a113f09dc
bartelsmichael5 <bartelsmichael5@gmail.com>
2020-10-01 23:41:32 +0000

    * add getAngleDegrees, degreesToRadians, radiansToDegrees and getDistance methods

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
 (function(window, _) {
   window.myLibraryName = window.myLibraryName || {
-    numz: {},
-    phyz: {},
+    numz: {
+        getAngleDegrees(pointA, pointB) {
+            const   
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                radians = Math.atan2(distanceY, distanceX),
+                degrees = radians * 180 / Math.PI;
+            return degrees;
+        },
+        degreesToRadians(degrees) {
+            return degrees * Math.PI / 180;
+        },
+        radiansToDegrees(radians) {
+            return radians * 180 / Math.PI;
+        }
+    },
+    phyz: {
+        getDistance(pointA, pointB) {
+            const
+                distanceX = pointB.x - pointA.x,
+                distanceY = pointB.y - pointA.y,
+                distance = Math.sqrt(distanceX * distanceX + distanceY * distanceY);
+            return distance;
+        }
+    },
   };
 }(window, window._));
